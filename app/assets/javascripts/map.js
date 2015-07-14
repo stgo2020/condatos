d6f81ab218e456a745efe2d5647cb0517c28a012
Sebas <sebastian.salinas.p@gmail.com>
2015-07-14 00:11:47 -0400

    intento

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,7 @@
 
  /////////////////////////////////////////// Obtener info desde modelo /tracks/id/points.json /////////////////////////////////////////
 
-  $.getJSON('http://192.168.0.20/tracks/' + gon.track_number.id + '/points.json', function (data){
+  $.getJSON('http://rubiapp.herokuapp.com/tracks/' + gon.track_number.id + '/points.json', function (data){
 	elemento = data;
 	x = new Array(elemento.length);                     // Arreglo x mantiene todas las coordenadas del metodo latlng 
 	
