01f530f38776e0eec8e7cf85b38abd1f487c052b
Sebas <sebastian.salinas.p@gmail.com>
2015-07-29 20:06:20 -0400

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap
 //= require jquery_ujs
 //= require raphael
 //= require morris
 //= require_tree .
-//= require bootstrap
