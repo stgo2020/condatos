c494dd6432025e773ee57f2fd1f3dbd0412985e1
Sebas <sebastian.salinas.p@gmail.com>
2015-08-02 00:46:19 -0400

    beta 3

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -224,7 +224,7 @@
 
 //////////////////////////////// Interaccion con Mapa ////////////////////////////////////////////////////
 
-  marker =  L.marker([0, 0], {    // Marcador viajero
+  marker =  L.marker([0, 0], {        // Definir Marcador viajero
   icon: L.mapbox.marker.icon({
     'marker-size': 'medium',          // https://www.mapbox.com/guides/an-open-platform/#simplestyle
     'marker-color': '#3885d4',
@@ -234,8 +234,64 @@
   })
 }).addTo(map);
 
-////////////////////////////// Tablas de Estadisticas /////////////////////////////////////////////////////
+
+});
+
+////////////////////////////// Tablas de Velocidad vs Tiempo  /////////////////////////////////////////////////////
+////////////////////////////// Dibujar Marcador Viajero       /////////////////////////////////////////////////////
+
+$(function () {$.getJSON('/tracks.json', function (data) {
 
 
+        $('#container').highcharts({
+
+             plotOptions: {
+                crosshairs: true,
+             series: {
+                point: {
+                    events: {
+                        mouseOver: function () {
+                            var chart = this.series.chart;
+                            vel = this.y;
+                            tie = this.x;
+                            for (var i = 0; i < info_v.length; i++) {           // Loop para identificar id del punto y redirigir al sitio 
+                                if (info_v[i][1] == vel && info_v[i][0]== tie){          
+                                    marker.setLatLng([xf[i][0],xf[i][1]])
+                                    marker.addTo(map);
+                                    break;
+                                    }
+                                }   
+                            }
+                        }
+                    },
+                    events: {
+                        mouseOut: function () {
+                            }
+                       }
+                    }
+                },
+            chart: {
+                zoomType: 'x',
+            },
+            title: {
+                text: ' '
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                min: 0,
+                max: 40,
+            },
+            legend: {
+                enabled: false
+            },
+            series: [{
+                name: 'Velocidad [Km/hr]',
+                data: info_v,
+                type: 'area',
+            }]
+        });
+    });
 });
 
