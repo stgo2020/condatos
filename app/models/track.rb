5662df2768e4e8aee1ad8786ccdcba39438f31d9
Sebas <sebastian.salinas.p@gmail.com>
2015-08-16 22:54:09 -0400

    mejoras 2

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,8 +1,8 @@
 class Track < ActiveRecord::Base
 	
-	belongs_to :user, dependent: :destroy
+	belongs_to :user
 	validates_presence_of :user
-	has_many :points
+	has_many :points, dependent: :destroy
 	validates_presence_of :name
 	accepts_nested_attributes_for :points
 	
